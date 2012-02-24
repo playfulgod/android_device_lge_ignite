2bdb0ca49a270e1e2e5c0e656d1a7ab4b04cf1e7
playfulgod <playfulgod@gmail.com>
2012-02-24 17:36:25 -0500

    booting, but cwm boot only.

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,2 +1,2 @@
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/device_ignite.mk
+    $(LOCAL_DIR)/ignite.mk
---
+++ ignite.mk
@@ -0,0 +1,37 @@
+$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
+
+# The gps config appropriate for this device
+$(call inherit-product, device/common/gps/gps_us_supl.mk)
+
+$(call inherit-product-if-exists, vendor/lge/ignite/ignite-vendor.mk)
+
+DEVICE_PACKAGE_OVERLAYS += device/lge/ignite/overlay
+
+
+ifeq ($(TARGET_PREBUILT_KERNEL),)
+	LOCAL_KERNEL := device/lge/ignite/kernel
+else
+	LOCAL_KERNEL := $(TARGET_PREBUILT_KERNEL)
+endif
+
+PRODUCT_COPY_FILES += \
+    $(LOCAL_KERNEL):kernel
+
+# Recovery Files
+PRODUCT_COPY_FILE += \
+    device/lge/ignite/recovery/recovery.rc:recovery/root/recovery.rc \
+    device/lge/ignite/ueventd.rc:recovery/root/ueventd.rc \
+    device/lge/ignite/recovery/postrecoveryboot.sh:recovery/root/postrecoveryboot.sh 
+
+$(call inherit-product, build/target/product/full.mk)
+
+PRODUCT_BUILD_PROP_OVERRIDES += BUILD_UTC_DATE=0
+PRODUCT_NAME := full_ignite
+PRODUCT_DEVICE := ignite
+
+PRODUCT_PROPERTY_OVERRIDES += \
+	persist.hidden_reset=0 \
+	persist.recovery_skip=0 \
+	persist.hiddenreset.hidden_reset=0 \
+	persist.hiddenreset.recovery_skip=0 \
+	persist.hiddenreset.firstboot=0
